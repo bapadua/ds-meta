88dd7b9253c38a944af1155621c1508791814871
Bruno Alves de Padua <bruno.padua@programmers.com.br>
2022-07-13 13:17:43 -0300

    project created

---
+++ backend/src/test/kotlin/io/github/bapadua/dsmeta/DsmetaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.bapadua.dsmeta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
